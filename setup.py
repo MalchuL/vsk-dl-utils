714f9d16d2881d309f1d6ccf661f99c9e49bd5a4
malchul <vladislaw.sorockin@yandex.ru>
2024-03-31 22:23:12 +0300

    Fix imports

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='vsk-dl-utils',
-    version='0.0.2',
+    version='0.0.3',
     description='My utils to train deep learning models',
     author='Vladislav Sorokin',
-    packages=['vsk_dl_utils'],
+    packages=find_packages(include=['vsk_dl_utils', 'vsk_dl_utils.*']),
     install_requires=['torch==2.2.1',
                       'torchvision==0.17.1',
                       'torchmetrics>=0.11.4',
